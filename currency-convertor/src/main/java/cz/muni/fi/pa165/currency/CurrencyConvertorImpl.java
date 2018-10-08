7e8deb840f1debd39b698d0eb48b56a1e6b13bce
rajivvairo26 <486938@mail.muni.cz>
2018-10-08 13:08:46 +0200

    task3

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
@@ -1,7 +1,13 @@
 package cz.muni.fi.pa165.currency;
 
+
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.math.RoundingMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 
 /**
@@ -12,7 +18,7 @@ import java.util.Currency;
 public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     private final ExchangeRateTable exchangeRateTable;
-    //private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
+    private final static Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
 
     public CurrencyConvertorImpl(ExchangeRateTable exchangeRateTable) {
         this.exchangeRateTable = exchangeRateTable;
@@ -20,7 +26,27 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        logger.trace("convert({},{},{})",sourceCurrency, targetCurrency, sourceAmount);
+        if (sourceCurrency == null) {
+            throw new IllegalArgumentException("sourceCurrency is null");
+        }
+        if (targetCurrency == null) {
+            throw new IllegalArgumentException("targetCurrency is null");
+        }
+        if (sourceAmount == null) {
+            throw new IllegalArgumentException("sourceAmount is null");
+        }
+        try {
+            BigDecimal exchangeRate = exchangeRateTable.getExchangeRate(sourceCurrency, targetCurrency);
+            if (exchangeRate == null) {
+                throw new UnknownExchangeRateException("ExchangeRate is unknown");
+            }
+            return exchangeRate.multiply(sourceAmount).setScale(2, RoundingMode.HALF_EVEN);
+        } catch (ExternalServiceFailureException ex) {
+            throw new UnknownExchangeRateException("Error when fetching exchange rate", ex);
+        }
+       
+
     }
 
 }
--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -2,38 +2,82 @@ package cz.muni.fi.pa165.currency;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.math.BigDecimal;
+import java.util.Currency;
+import static org.junit.Assert.*;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CurrencyConvertorImplTest {
+    private static Currency CZK = Currency.getInstance("CZK");
+    private static Currency IDR = Currency.getInstance("IDR");
+
+    @Mock
+    private ExchangeRateTable exchangeRateTable;
+
+    private CurrencyConvertor currencyConvertor;
+    
+    @Before
+    public void init() {
+        currencyConvertor = new CurrencyConvertorImpl(exchangeRateTable);
+    }
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
-    public void testConvert() {
+    public void testConvert() throws ExternalServiceFailureException {
         // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
+        when(exchangeRateTable.getExchangeRate(CZK, IDR))
+                .thenReturn(new BigDecimal("0.1"));
+        assertEquals(new BigDecimal("1.00"), currencyConvertor.convert(CZK, IDR, new BigDecimal("10.050")));
+        assertEquals(new BigDecimal("1.01"), currencyConvertor.convert(CZK, IDR, new BigDecimal("10.051")));
+        assertEquals(new BigDecimal("1.01"), currencyConvertor.convert(CZK, IDR, new BigDecimal("10.149")));
+        assertEquals(new BigDecimal("1.02"), currencyConvertor.convert(CZK, IDR, new BigDecimal("10.150")));
     }
 
     @Test
     public void testConvertWithNullSourceCurrency() {
-        fail("Test is not implemented yet.");
+       // fail("Test is not implemented yet.");
+       expectedException.expect(IllegalArgumentException.class);
+        currencyConvertor.convert(null, IDR, BigDecimal.ONE);
     }
 
     @Test
     public void testConvertWithNullTargetCurrency() {
-        fail("Test is not implemented yet.");
+       // fail("Test is not implemented yet.");
+       expectedException.expect(IllegalArgumentException.class);
+        currencyConvertor.convert(CZK, null, BigDecimal.ONE);
     }
 
     @Test
     public void testConvertWithNullSourceAmount() {
-        fail("Test is not implemented yet.");
+       // fail("Test is not implemented yet.");
+       expectedException.expect(IllegalArgumentException.class);
+        currencyConvertor.convert(CZK, IDR, null);
     }
 
     @Test
-    public void testConvertWithUnknownCurrency() {
-        fail("Test is not implemented yet.");
+    public void testConvertWithUnknownCurrency()throws ExternalServiceFailureException {
+       // fail("Test is not implemented yet.");
+       when(exchangeRateTable.getExchangeRate(CZK, IDR))
+                .thenReturn(null);
+        expectedException.expect(UnknownExchangeRateException.class);
+        currencyConvertor.convert(CZK, IDR, BigDecimal.ONE);
     }
 
     @Test
-    public void testConvertWithExternalServiceFailure() {
-        fail("Test is not implemented yet.");
+    public void testConvertWithExternalServiceFailure() throws ExternalServiceFailureException {
+        when(exchangeRateTable.getExchangeRate(CZK, IDR))
+                .thenThrow(UnknownExchangeRateException.class);
+        expectedException.expect(UnknownExchangeRateException.class);
+        currencyConvertor.convert(CZK, IDR, BigDecimal.ONE);
+         //fail("Test is not implemented yet.");
     }
 
 }
